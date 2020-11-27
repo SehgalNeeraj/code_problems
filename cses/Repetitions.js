0b13b955232ed64c06666b86417e539b92f31d62
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2020-11-27 11:49:52 +0530

    done

--- cses/Repetitions.js
+++ cses/Repetitions.js
@@ -1,23 +1,39 @@
-// You are given a DNA sequence: a string consisting of characters A, C, G, and T. Your task is to find the longest repetition in the sequence. This is a maximum-length substring containing only one type of character.
+// You are given a DNA sequence: a string consisting of characters A, C, G,
+// and T.
+// Your task is to find the longest repetition in the sequence.
+// This is a maximum - length substring containing only one type of
+// character.
+//
 // Input
 // The only input line contains a string of n characters.
+//
 // Output
 // Print one integer: the length of the longest repetition.
+//
 // Constraints
 // 1≤n≤106
+//
 // Example
 // Input:
 // ATTCGGGA
 // Output:
 // 3
+
 ////////////////////////////////////////////////////////////////////////
-function repititions(n) {
-    
+function repetitions(str) {
+  let longest_rep = 0;
+  let current_rep = 1;
+
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] === str[i + 1]) {
+      current_rep++;
+    } else {
+      longest_rep = longest_rep < current_rep ? current_rep : longest_rep;
+      current_rep = 1;
+    }
+  }
+  console.log(longest_rep);
 }
-
-
-
-
 
 ////////////////////////
 ////// MAIN
@@ -29,6 +45,6 @@ const readline = require("readline").createInterface({
 
 // MAIN
 readline.question("", (input) => {
-  repetitions(parseInt(input));
+  repetitions(input);
   readline.close();
 });
