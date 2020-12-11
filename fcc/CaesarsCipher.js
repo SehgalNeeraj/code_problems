b0d9ecd15b7a5aa2917381562d3ac3f6b33cb337
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2020-12-11 09:44:45 +0530

    done

--- fcc/CaesarsCipher.js
+++ fcc/CaesarsCipher.js
@@ -11,3 +11,53 @@
 
 // e.g,
 // rot13("SERR PBQR PNZC") should decode to FREE CODE CAMP
+
+// rot13("SERR CVMMN!") should decode to FREE PIZZA!
+
+// rot13("SERR YBIR?") should decode to FREE LOVE?
+
+// rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.") should decode to
+// THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.
+function isAlpha(character) {
+    // #TODO: maybe use RegEX instead???
+  if (character.toLowerCase() != character.toUpperCase()) {
+    return true;
+  }
+  return false;
+}
+
+function decode(character) {
+  // bound b/w A <-> Z
+  let ascii = character.charCodeAt() - 13;
+  if (ascii < "A".charCodeAt()) {
+    ascii = "Z".charCodeAt() - ("A".charCodeAt() - ascii) + 1;
+  }
+
+  return String.fromCharCode(ascii);
+}
+
+function rot13(input) {
+  // 1. iterate thru input
+  // 2. ignore non-alpha
+  // 3. decode logic
+  // 4. print
+  let response = "";
+  for (let i = 0; i < input.length; ++i) {
+    response += isAlpha(input[i]) ? decode(input[i].toUpperCase()) : input[i];
+  }
+  console.log(response);
+}
+
+////////////////////////
+////// MAIN
+////////////////////////
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// MAIN
+readline.question("", (input) => {
+  rot13(input);
+  readline.close();
+});
