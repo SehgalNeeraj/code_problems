b584addc3b7f98b7e37e1d345189e7026ffddced
Neeraj Sehgal <neeraj.sehgal@gmail.com>
2020-12-21 10:25:26 +0530

    chapter1:sum

---
+++ cses/dataStar/sum.js
@@ -0,0 +1,23 @@
+// Your task is to calculate the sum of two integers.
+
+// Input: The only line in the input contains two integersa and b.
+
+// Output: Your program should print the amounta + b.
+
+// Bounds
+// - 2 ⋅109≤ a , b ≤ 2 ⋅109
+// Example
+
+// Input:
+// 3 5
+
+// Output:
+// 8
+////////////////////////////////////////////////////////////////////////
+// Main
+////////////////////////////////////////////////////////////////////////
+function sum(a, b) {
+  return parseInt(a) + parseInt(b);
+}
+
+console.log(sum(3, 5));
